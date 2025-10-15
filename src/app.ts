34ad3b0cc76f13f4f6eb12fdc95d8dc5f3a9d6c1
arjunraj <arjunraj.e@reizend.ai>
2025-10-15 16:19:54 +0530

    production

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import path from "path"
 
 const app=express()
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://shope-fullstack.onrender.com',
   credentials: true
 }));
 app.use(express.json());
