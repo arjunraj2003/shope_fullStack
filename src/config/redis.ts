5aa650e31e989033cfd681635a41fb352a50650b
arjunraj <arjunraj.e@reizend.ai>
2025-10-15 16:42:59 +0530

    production

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,16 +1,30 @@
+// import { createClient } from "redis";
+
+// const redis=createClient(
+//      {
+//         socket: {
+//         host: 'localhost',  // or your Redis host/IP or container name
+//         port: 6379,         // or your Redis port
+//         timeout: 10000      // optional: increase timeout to 10s
+//         }
+//      }
+// );
+
+// redis.on("error",(err)=>console.log("Redis error:",err));
+
+// redis.connect();
+
+// export default redis;
+
 import { createClient } from "redis";
 
-const redis=createClient(
-     {
-        socket: {
-        host: 'localhost',  // or your Redis host/IP or container name
-        port: 6379,         // or your Redis port
-        timeout: 10000      // optional: increase timeout to 10s
-        }
-     }
-);
+const redis = createClient({
+  url: process.env.REDIS_URL,
+});
 
-redis.on("error",(err)=>console.log("Redis error:",err));
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
 
 redis.connect();
 
