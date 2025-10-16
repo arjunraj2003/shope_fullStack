183d21575b845da402d52e43a0c3df3ecca1899e
arjunraj <arjunraj.e@reizend.ai>
2025-10-16 12:01:38 +0530

    cloudinary implemented

--- src/controllers/dress.controller.ts
+++ src/controllers/dress.controller.ts
@@ -2,67 +2,72 @@ import { NextFunction, Request, Response } from "express";
 import { DressOperations } from "../services/dress.service";
 import { ApiResponse } from "../utils/ApiResponse";
 
-export class DressController{
-    static async createDress(req:Request,res:Response,next:NextFunction){
-        try{
-            const userId=(req as any).user?.id;
-            const data=req.body
-            const files=req.files as Express.Multer.File[];
+export class DressController {
+    static async createDress(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = (req as any).user?.id;
+            const data = req.body;
+            const files = req.files as Express.Multer.File[];
 
-            const imageUrls=files.map((file)=>`/uploads/dresses/${file.filename}`);
-            
-            const dress=await DressOperations.createDress(data,userId,imageUrls);
-            res.status(201).json(new ApiResponse(true,"Dress created successfully",dress));
-        }catch(error){
-            next(error)
-        }
-    }
+         
+            const images = files.map((file) => (file.path));
 
-    static async getAllDress(req:Request,res:Response,next:NextFunction){
-        try{
-            const filters=req.query;
-            const dresses=await DressOperations.getAllDress(filters);
-          
-            res.status(200).json(new ApiResponse(true,"Dress Fetched successfully ",dresses));
-        }catch(error){
+            const dress = await DressOperations.createDress(data, userId, images);
+
+            res.status(201).json(
+                new ApiResponse(true, "Dress created successfully", dress)
+            );
+        } catch (error) {
             next(error);
         }
     }
 
-    static async getDressById(req:Request,res:Response,next:NextFunction){
-        try{
-            const id=req.params.id;
-            
-            const dress=await DressOperations.getDressById(id);
-            res.status(200).json(new ApiResponse(true,"Dress Fetch successfully",dress));
-        }catch(error){
+
+    static async getAllDress(req: Request, res: Response, next: NextFunction) {
+        try {
+            const filters = req.query;
+            const dresses = await DressOperations.getAllDress(filters);
+
+            res.status(200).json(new ApiResponse(true, "Dress Fetched successfully ", dresses));
+        } catch (error) {
             next(error);
         }
     }
 
-    static async updateDress(req:Request,res:Response,next:NextFunction){
-        try{
-            const userId=(req as any).user.id;
-            const dressId=req.params.id;
-            const data=req.body;
-            const files=req.files as Express.Multer.File[];
+    static async getDressById(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = req.params.id;
 
-            const imageUrls=files.map((file)=>`/uploads/dresses/${file.filename}`);
-            const dress=await DressOperations.updateDressById(dressId,data,userId,imageUrls);
-            res.status(200).json(new ApiResponse(true,"Dress Updated successfully",dress))
-        }catch(error){
+            const dress = await DressOperations.getDressById(id);
+            res.status(200).json(new ApiResponse(true, "Dress Fetch successfully", dress));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async updateDress(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = (req as any).user.id;
+            const dressId = req.params.id;
+            const data = req.body;
+            const files = req.files as Express.Multer.File[];
+
+            const imageUrls = files.map((file) => `/uploads/dresses/${file.filename}`);
+            const dress = await DressOperations.updateDressById(dressId, data, userId, imageUrls);
+            res.status(200).json(new ApiResponse(true, "Dress Updated successfully", dress))
+        } catch (error) {
             next(error);
             console.log(error);
         }
     }
 
-    static async deleteDress(req:Request,res:Response,next:NextFunction){
-        try{
-            const dressId=req.params.id;
-            const userId=(req as any).user.id;
-            const dress=await DressOperations.deleteDress(dressId,userId);
-            res.status(200).json(new ApiResponse(true,"Dress Deleted successfully",dress))
-        }catch(error){
+    static async deleteDress(req: Request, res: Response, next: NextFunction) {
+        try {
+            const dressId = req.params.id;
+            const userId = (req as any).user.id;
+            const dress = await DressOperations.deleteDress(dressId, userId);
+            res.status(200).json(new ApiResponse(true, "Dress Deleted successfully", dress))
+        } catch (error) {
             next(error);
 
         }
