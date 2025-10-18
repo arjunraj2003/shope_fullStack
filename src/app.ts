94cd1e073f68cc70dabe08eb7ea5742f74ba9858
arjunraj <arjunraj.e@reizend.ai>
2025-10-18 10:17:42 +0530

    cors error

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import path from "path"
 
 const app=express()
 app.use(cors({
-  origin: 'http://https://shope-fullstack.onrender.com:3000',
+  origin: 'http://https://shope-fullstack.onrender.com',
   credentials: true
 }));
 app.use(express.json());
